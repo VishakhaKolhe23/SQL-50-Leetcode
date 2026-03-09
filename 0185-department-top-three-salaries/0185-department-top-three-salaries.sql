466dc664f993a8a3ed07f71cedc28af35db3be6a
vishakhakolhe23 <vishakhakolhe321@gmail.com>
2026-03-09 23:26:52 +0530

    Time: 920 ms (59.81%), Space: 0B (100%) - LeetHub

---
+++ 0185-department-top-three-salaries/0185-department-top-three-salaries.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+select department ,employee, salary from
+(select d.name as Department , e.name as Employee ,e.salary as Salary,
+dense_rank() over (partition by d.name order by e.salary desc) as ranks
+from employee e
+left join department d
+on e.departmentId=d.id) temp
+where ranks <= 3
+
+
+

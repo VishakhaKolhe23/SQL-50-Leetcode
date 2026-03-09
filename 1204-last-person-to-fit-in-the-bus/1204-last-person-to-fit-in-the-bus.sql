bc40c1676a2ebf67320ee295d4d8dcd965cb9d3c
vishakhakolhe23 <vishakhakolhe321@gmail.com>
2026-03-09 23:22:01 +0530

    Time: 1416 ms (21.9%), Space: 0B (100%) - LeetHub

---
+++ 1204-last-person-to-fit-in-the-bus/1204-last-person-to-fit-in-the-bus.sql
@@ -0,0 +1,12 @@
+# Write your MySQL query statement below
+select q1.person_name
+from Queue q1
+inner join queue q2
+on q1.turn >= q2.turn
+group by q1.turn
+having sum(q2.weight) <=1000
+order by q1.turn desc limit 1 #returns 1 last row
+
+
+
+

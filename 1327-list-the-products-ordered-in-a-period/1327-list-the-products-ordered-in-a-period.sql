8b836f53274bd9c4b1074bf8721944fcf417d900
vishakhakolhe23 <vishakhakolhe321@gmail.com>
2026-03-09 23:32:23 +0530

    Time: 659 ms (85.14%), Space: 0B (100%) - LeetHub

---
+++ 1327-list-the-products-ordered-in-a-period/1327-list-the-products-ordered-in-a-period.sql
@@ -0,0 +1,7 @@
+SELECT p.product_name, SUM(o.unit) AS unit
+FROM Products p
+JOIN Orders o
+ON p.product_id = o.product_id
+WHERE EXTRACT(YEAR_MONTH FROM o.order_date) = 202002
+GROUP BY p.product_name
+HAVING SUM(o.unit) >= 100;
